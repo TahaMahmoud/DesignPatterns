96599138a00b2fed4f732a116799f1f81574a47f
Taha <Taha.Mahmoud@must.edu.eg>
2021-10-31 16:41:26 +0200

    Command

--- Behavioral/Command.playground/Contents.swift
+++ Behavioral/Command.playground/Contents.swift
@@ -1,3 +1,66 @@
 // Command Pattern
 
+protocol Command {
+    func execute()
+}
 
+class JoystickInvoker {
+    
+    var command: Command?
+    
+    func setCommand(command: Command) {
+        self.command = command
+    }
+    
+    func doAction() {
+        command?.execute()
+    }
+}
+
+class LongPass: Command {
+    
+    var player = Player()
+
+    func execute() {
+        player.doLongPass()
+    }
+    
+}
+
+class Tackling: Command {
+    
+    var player = Player()
+
+    func execute() {
+        player.doTackling()
+    }
+    
+}
+
+class Player {
+    
+    func hasBall() -> Bool {
+        return true
+    }
+    
+    func doLongPass() {
+        print("Doing LongPass")
+    }
+    
+    func doTackling() {
+        print("Doing Tackling")
+    }
+    
+}
+
+let joystickInvoker = JoystickInvoker()
+let player = Player()
+
+if player.hasBall() {
+    joystickInvoker.setCommand(command: LongPass())
+}
+else {
+    joystickInvoker.setCommand(command: Tackling())
+}
+
+joystickInvoker.doAction()
