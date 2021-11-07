699ef2e2a1f636ffbb321751d2822901f807dff8
Taha <Taha.Mahmoud@must.edu.eg>
2021-11-07 10:02:29 +0200

    Template Method

---
+++ Behavioral/Template Method.playground/Contents.swift
@@ -0,0 +1,88 @@
+// Template Method Pattern
+
+protocol AbstractProtocol {
+
+    func templateMethod()
+
+    func baseOperation1()
+
+    func baseOperation2()
+
+    func baseOperation3()
+
+    func requiredOperations1()
+    func requiredOperation2()
+
+    func hook1()
+    func hook2()
+}
+
+extension AbstractProtocol {
+
+    func templateMethod() {
+        baseOperation1()
+        requiredOperations1()
+        baseOperation2()
+        hook1()
+        requiredOperation2()
+        baseOperation3()
+        hook2()
+    }
+
+    func baseOperation1() {
+        print("AbstractProtocol says: I am doing the bulk of the work\n")
+    }
+
+    func baseOperation2() {
+        print("AbstractProtocol says: But I let subclasses override some operations\n")
+    }
+
+    func baseOperation3() {
+        print("AbstractProtocol says: But I am doing the bulk of the work anyway\n")
+    }
+
+    func hook1() {}
+    func hook2() {}
+}
+
+class ConcreteClass1: AbstractProtocol {
+
+    func requiredOperations1() {
+        print("ConcreteClass1 says: Implemented Operation1\n")
+    }
+
+    func requiredOperation2() {
+        print("ConcreteClass1 says: Implemented Operation2\n")
+    }
+
+    func hook2() {
+        print("ConcreteClass1 says: Overridden Hook2\n")
+    }
+}
+
+class ConcreteClass2: AbstractProtocol {
+
+    func requiredOperations1() {
+        print("ConcreteClass2 says: Implemented Operation1\n")
+    }
+
+    func requiredOperation2() {
+        print("ConcreteClass2 says: Implemented Operation2\n")
+    }
+
+    func hook1() {
+        print("ConcreteClass2 says: Overridden Hook1\n")
+    }
+}
+
+class Client {
+    static func clientCode(use object: AbstractProtocol) {
+        object.templateMethod()
+    }
+}
+
+print("Same client code can work with different subclasses:\n")
+Client.clientCode(use: ConcreteClass1())
+
+print("\nSame client code can work with different subclasses:\n")
+Client.clientCode(use: ConcreteClass2())
